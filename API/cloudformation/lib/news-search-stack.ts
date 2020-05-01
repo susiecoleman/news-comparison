22ffb3619241130e51fb987770d4686351ad8ddc
Susie Coleman <git@susiecoleman.me>
2020-05-01 15:48:41 +0100

    build: Create User to deploy Lambda with GitHub Actions

--- API/cloudformation/lib/news-search-stack.ts
+++ API/cloudformation/lib/news-search-stack.ts
@@ -1,6 +1,21 @@
-import { Stack, Construct, StackProps, Tag, App } from "@aws-cdk/core";
+import {
+  Stack,
+  Construct,
+  StackProps,
+  Tag,
+  App,
+  CfnOutput,
+  RemovalPolicy,
+} from "@aws-cdk/core";
 import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
 import { Bucket, BucketAccessControl } from "@aws-cdk/aws-s3";
+import {
+  PolicyStatement,
+  Effect,
+  User,
+  Policy,
+  CfnAccessKey,
+} from "@aws-cdk/aws-iam";
 
 class NewsSearchStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,17 +23,44 @@ class NewsSearchStack extends Stack {
 
     Tag.add(this, "app", "news-search-api");
 
-    new Bucket(this, "NewsSearchLambdaCodeBucket", {
+    const bucket = new Bucket(this, "NewsSearchLambdaCodeBucket", {
       bucketName: "news-lambda-code-repo",
       accessControl: BucketAccessControl.PRIVATE,
+      removalPolicy: RemovalPolicy.DESTROY,
     });
 
     // Initially create an empty function for the code source.
     // The deployment process deploys the code in the "NewsSearchLambdaCodeBucket" to the lambda
-    new Function(this, "NewsSearchLambda", {
+    const lambda = new Function(this, "NewsSearchLambda", {
       runtime: Runtime.NODEJS_12_X,
       handler: "dist.index.handler",
       code: Code.fromInline("exports.handler = function(event,context){}"),
+    });
+
+    const githubAction: User = new User(this, "GitHubAction", {});
+
+    const deployLambda: Policy = new Policy(this, "DeployLambda", {});
+    deployLambda.addStatements(
+      new PolicyStatement({
+        resources: [`${bucket.bucketArn}/*`, bucket.bucketArn],
+        actions: ["s3:PutObject", "s3:GetObject"],
+        effect: Effect.ALLOW,
+      }),
+      new PolicyStatement({
+        resources: [lambda.functionArn],
+        actions: ["lambda:UpdateFunctionCode"],
+        effect: Effect.ALLOW,
+      })
+    );
+    deployLambda.attachToUser(githubAction);
+
+    const accessKeys = new CfnAccessKey(this, "DeployKeys", {
+      userName: githubAction.userName,
+    });
+
+    new CfnOutput(this, "AccessKey", { value: accessKeys.ref });
+    new CfnOutput(this, "Secret Access Key", {
+      value: accessKeys.attrSecretAccessKey,
     });
   }
 }
