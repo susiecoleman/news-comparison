79e492e0adf8bf08728d825388440820bf73ee40
Susie Coleman <git@susiecoleman.me>
2020-11-18 21:37:10 +0000

    Fix news sources. Use valid sources

--- public/script.js
+++ public/script.js
@@ -1,38 +1,40 @@
-$("#searchTerm").keypress(function(event) {
-    if (event.which == 13) {
-        getSearchValue();
-    }
+$("#searchTerm").keypress(function (event) {
+  if (event.which == 13) {
+    getSearchValue();
+  }
 });
 
 function getSearchValue() {
-    searchTerm = $("#searchTerm").val();
-    $("#searchTerm").val("");
-    $("#sources").empty();
-    $("#sources").append(
-        '<h1 class="grid--box-3_column_span" id="sources">Sources:</h1>' +
-        '<h1 class="grid--box-3_column_span">Most relevant articles on ' + searchTerm + '</h1>'
-    );
-    getArticle(searchTerm, "daily-mail");
-    getArticle(searchTerm, "the-guardian-uk");
-    getArticle(searchTerm, "bbc-news");
-    getArticle(searchTerm, "independent");
-    getArticle(searchTerm, "metro");
-    getArticle(searchTerm, "mirror");
-    getArticle(searchTerm, "the-telegraph");
-    getArticle(searchTerm, "the-economist");
-    getArticle(searchTerm, "financial-times");
+  searchTerm = $("#searchTerm").val();
+  $("#searchTerm").val("");
+  $("#sources").empty();
+  $("#sources").append(
+    '<h1 class="grid--box-3_column_span" id="sources">Sources:</h1>' +
+      '<h1 class="grid--box-3_column_span">Most relevant articles on ' +
+      searchTerm +
+      "</h1>"
+  );
+  getArticle(searchTerm, "abc-news");
+  getArticle(searchTerm, "al-jazeera-english");
+  getArticle(searchTerm, "bbc-news");
+  getArticle(searchTerm, "buzzfeed");
+  getArticle(searchTerm, "fox-news");
+  getArticle(searchTerm, "independent");
+  getArticle(searchTerm, "reuters");
+  getArticle(searchTerm, "the-globe-and-mail");
+  getArticle(searchTerm, "the-irish-times");
 }
 
 function getArticle(searchTerm, source) {
-    $.post(
-        '/article',
-        JSON.stringify({
-          'searchTerm': searchTerm,
-          'source': source
-        })
-    ).then(function(data){
-        $("#" + source + "_headline").text(data.headline);
-        $("#" + source + "_link").attr("href", data.url);
-        $("#" + source + "_image").attr("src", data.image);
+  $.post(
+    "/article",
+    JSON.stringify({
+      searchTerm: searchTerm,
+      source: source,
     })
+  ).then(function (data) {
+    $("#" + source + "_headline").text(data.headline);
+    $("#" + source + "_link").attr("href", data.url);
+    $("#" + source + "_image").attr("src", data.image);
+  });
 }
--- src/api.js
+++ src/api.js
@@ -1,19 +1,26 @@
 var fetch = require("node-fetch");
 
 function articleResponse(searchTerm, source) {
-    const url = "https://newsapi.org/v2/everything?apiKey=" + process.env.NEWSAPI_API_KEY +
-    "&sortBy=relevancy&language=en&sources=" + source + "&q=" + searchTerm
-    return fetch(url)
-        .then(function(response){
-            return response.json();
-        }).then(function(json){
-            var article = json.articles[0];
-            var content = {};
-            content["headline"] = article.title;
-            content["image"] = article.urlToImage;
-            content["url"] = article.url;
-            return content;
-        });
+  const url =
+    "https://newsapi.org/v2/everything?apiKey=" +
+    process.env.NEWSAPI_API_KEY +
+    "&sortBy=relevancy&language=en&sources=" +
+    source +
+    "&q=" +
+    searchTerm;
+  console.log(url);
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      var article = json.articles[0];
+      var content = {};
+      content["headline"] = article.title;
+      content["image"] = article.urlToImage;
+      content["url"] = article.url;
+      return content;
+    });
 }
 
-module.exports = articleResponse
+module.exports = articleResponse;
